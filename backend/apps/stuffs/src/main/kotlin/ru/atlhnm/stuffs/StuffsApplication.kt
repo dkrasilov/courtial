fbc69508cf0bb7c51fbf12b05bcfcca41d741f53
Dima Krasilov <dfkrasilov@gmail.com>
2021-02-01 02:22:39 +0300

    migrate to gradle and create sample stuffs app

---
+++ backend/apps/stuffs/src/main/kotlin/ru/atlhnm/stuffs/StuffsApplication.kt
@@ -0,0 +1,11 @@
+package ru.sfera.telephony
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StuffsApplication
+
+fun main(args: Array<String>) {
+	runApplication<StuffsApplication>(*args)
+}
